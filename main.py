cbfe6c831e1d2da4a5607433eb2d653d66734d22
Егор Кондрашов <egorkondrasov@Egors-MacBook-Pro.local>
2024-06-17 13:12:11 +0300

    add summary auto-delete

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import logging
-import os
+import threading
+import time
 from datetime import datetime
 
 import telebot
+import schedule
+from schedule import repeat, every
 from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 
-
 import config
 from models import Message
-
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(format=config.format, encoding='utf-8',level=logging.INFO)
 
 model = config.mistral_model
 group_id = config.group_id
@@ -24,13 +22,19 @@ temperature = config.model_temperature
 max_tokens = config.max_tokens
 
 inital_prompt = config.initial_prompt
+delete_time = config.delete_message_time
+
+format = config.format
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format=format, encoding='utf-8',level=logging.INFO)
 
 bot = telebot.TeleBot(tg_api_key)
 client = MistralClient(mistral_api_key)
 
-logger.info('bot started')
-
 context = []
+messages_to_delete = {}
+
 
 @bot.message_handler(commands=['history'])
 def restore_messages(message: telebot.types.Message):
@@ -49,10 +53,14 @@ def restore_messages(message: telebot.types.Message):
             temperature=temperature,
             max_tokens=max_tokens
         )
-        bot.send_message(group_id, summary.choices[0].message.content)
+
+        summary = bot.send_message(group_id, summary.choices[0].message.content)
+        threading.Thread(target=delete_message, args=(summary.message_id, summary.chat.id)).start()
+        logger.info('code')
     except Exception as e:
         logger.error(f'Error generating summary: {e}')
-        bot.send_message(group_id, 'Sorry, I encountered an error while generating the summary.')
+        bot.send_message(group_id, 'Sorry, I encountered an error while generating the summary')
+
 
 
 @bot.message_handler(func=lambda message: True)
@@ -60,9 +68,17 @@ def main(message: telebot.types.Message):
     message_time = datetime.fromtimestamp(message.date)
     context.append(Message(timestamp=message_time, user_name=message.from_user.full_name, msg_text=message.text))
 
-
     if len(context) > max_messages:
         context.pop(0)
     
 
-bot.infinity_polling()
+def delete_message(message_id, chat_id):
+    time.sleep(60 * delete_time)
+    try:
+        bot.delete_message(chat_id, message_id)
+    except Exception as e:
+        logger.error(f"Error deleting message: {e}")
+
+if __name__ == "__main__":
+    logger.info('bot started')
+    bot.infinity_polling()
